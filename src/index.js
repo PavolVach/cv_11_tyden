56f8e3aeda8c5d8eed6284b3a2f0e0cffce5b5e5
PavolVach <118180440+PavolVach@users.noreply.github.com>
2022-11-28 13:50:34 +0100

    skolaa

--- src/index.js
+++ src/index.js
@@ -54,12 +54,54 @@ const light1 = new DirectionalLight(
 );
 
 //vytvoření cesty
+var points = [];
+var i;
+var n = 450; //pocet bodu
+var r = 50; //radius krivky
+for (i = 0; i < n + 1; i++) {
+  points.push(
+    new Vector3(
+      (r + (r / 5) * Math.sin((8 * i * Math.PI) / n)) *
+        Math.sin((4 * i * Math.PI) / n),
+      0,
+      (r + (r / 10) * Math.sin((6 * i * Math.PI) / n)) *
+        Math.cos((2 * i * Math.PI) / n)
+    )
+  );
+}
 
 //vykreslení křivky
+var track = MeshBuilder.CreateLines("track", { points });
+var freza = MeshBuilder.CreateCylinder("freza", { diameter: 0.0001 });
+SceneLoader.ImportMesh("", "public/", "endmill.glb", scene, function (
+  noveModely
+) {
+  freza = noveModely[0];
+  freza.scaling = new Vector3(0.15, 0.15, 0.25);
+  freza.position.y = 0;
+  freza.rotate(new Vector3(-1, 0, 0), Math.PI / 2);
+});
 
 //úhly a rotace
+var path3D = new Path3D(points);
+var normals = path3D.getNormals();
+var theta = Math.acos(Vector3.Dot(Axis.Z, normals[0]));
+freza.rotate(Axis.X, theta, Space.WORLD);
 
 //animace
+var i = 0;
+scene.registerAfterRender(function () {
+  freza.position.x = points[i].x;
+  freza.position.z = points[i].z;
+  theta = Math.acos(Vector3.Dot(normals[0], normals[i + 1]));
+  console.log(theta);
+  var sklopeni = Vector3.Cross(normals[i], normals[i + 1]).y;
+  sklopeni = sklopeni / Math.abs(sklopeni);
+  freza.rotate(Axis.Y, sklopeni * theta, Space.WORLD);
+  i = (i + 1) % (n - 1);
+});
+
+//fyzika
 
 // povinné vykreslování
 engine.runRenderLoop(function () {
